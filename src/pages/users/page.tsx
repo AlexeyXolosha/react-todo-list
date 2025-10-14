a478d5525d9184f0345eff12a563d2f6fc0dee20
AlexeyXolosha <alexeyxolosh4@gmail.com>
2025-10-14 17:59:01 +0500

    new step trans

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,4 +1,5 @@
-import {fetchUsers} from "../../shared/api.ts";
+import {createUser, fetchUsers} from "../../shared/api.ts";
+import {Suspense, useState, use} from "react";
 
 type User = {
     id: string;
@@ -8,32 +9,47 @@ type User = {
 const defaultUsersPromise = fetchUsers();
 
 export function UsersPage() {
+    const [usersPromise, setUserPromise] = useState(defaultUsersPromise);
+    const refetchUsers = () => setUserPromise(fetchUsers());
+
     return (
         <main className="container mx-auto p-4 pt-10 flex flex-col gap-4">
             <h1 className='text-3xl'>Users</h1>
             <section>
-                <CreateUserForm/>
+                <CreateUserForm refetchUsers={refetchUsers}/>
             </section>
-            <UsersList users={[
-                {id: '1', email: 'daddadad@gmail.com'},
-                {id: '2', email: 'dadasdadad2@mail.ru'}
-            ]}/>
+            <Suspense fallback={<div>Loading users...</div>}>
+                <UsersList usersPromise={usersPromise}/>
+            </Suspense>
         </main>
     );
 }
 
-export function CreateUserForm() {
+export function CreateUserForm({refetchUsers}: { refetchUsers?: () => void }) {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        await createUser({
+            email,
+            id: crypto.randomUUID(),
+        });
+        refetchUsers();
+        setEmail("");
+    };
+
     return (
-        <form className='flex gap-2 mb-2'>
-            <input className="border p-2 rounded" type="text"/>
+        <form className='flex gap-2 mb-2' onSubmit={handleSubmit}>
+            <input className="border p-2 rounded" type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg text-2xl'
-                    type='button'>Добавить
+                    type='submit'>Добавить
             </button>
         </form>
     )
 }
 
-export function UsersList({users}: { users: User[] }) {
+export function UsersList({usersPromise}: { usersPromise: Promise<User[]> }) {
+    const users = use(usersPromise);
     return (
         <section>
             <ul className="flex flex-col">
